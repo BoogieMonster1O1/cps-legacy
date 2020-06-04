e57b7a8aeb70e17ba4820d65ea30d2dbeca764e7
Shrish <harrypotter.starwars.shrish@gmail.com>
2020-06-04 19:32:34 +0530

    Fancy label

--- src/io/github/boogiemonster1o1/cpslegacy/Main.java
+++ src/io/github/boogiemonster1o1/cpslegacy/Main.java
@@ -1,10 +1,7 @@
 package io.github.boogiemonster1o1.cpslegacy;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import static java.lang.System.exit;
+import java.awt.*;
 
 public class Main extends JFrame {
 
@@ -12,6 +9,7 @@ public class Main extends JFrame {
     JMenuBar mainBar = new JMenuBar();
     JMenu cpsMenu1 = new JMenu();
     JMenuItem closeItem = new JMenuItem();
+    JLabel headingLabel = new JLabel("Clicks Per Second (Legacy edition)");
 
     public Main(){
         this.closeItem.setLabel("Close");
@@ -24,10 +22,16 @@ public class Main extends JFrame {
         this.setDefaultCloseOperation(3);
         this.setRootPane(rootPane);
         this.setSize(400,400);
+
+        this.headingLabel.setFont(new Font("Arial", Font.BOLD,20));
+        headingLabel.setBounds(10,10,350,25);
+        this.add(headingLabel);
+
         this.setVisible(true);
     }
     public static void main(String[] args){
         System.out.println("Starting ClicksPerSecond Legacy...");
+        System.out.println(System.getProperty("user.dir"));
         new Main();
     }
 
