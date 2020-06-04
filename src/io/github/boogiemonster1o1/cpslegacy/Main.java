47e6f0088f4914eccfbefc180d545fe327cd381c
Shrish <harrypotter.starwars.shrish@gmail.com>
2020-06-04 18:08:56 +0530

    Initial Commit

---
+++ src/io/github/boogiemonster1o1/cpslegacy/Main.java
@@ -0,0 +1,16 @@
+package io.github.boogiemonster1o1.cpslegacy;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    JMenuBar mainMenu = new JMenuBar();
+
+    public Main(){
+        this.setJMenuBar(mainMenu);
+        this.setLayout(null);
+    }
+    public static void main(String[] args){
+
+    }
+}
