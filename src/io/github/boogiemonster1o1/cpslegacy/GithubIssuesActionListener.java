64ad9ce3ad330e07b97ed85a3e2f7c2ff11208cc
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-06-05 20:46:53 +0530

    Removed annotations

--- src/io/github/boogiemonster1o1/cpslegacy/GithubIssuesActionListener.java
+++ src/io/github/boogiemonster1o1/cpslegacy/GithubIssuesActionListener.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GithubIssuesActionListener implements ActionListener {
-    @Override
     public void actionPerformed(ActionEvent actionEvent) {
         CPSLegacy.CPS.openBrowserWindow("https://github.com/BoogieMonster1O1/cps-legacy/issues");
     }
