4d453e1c16d698c99b1b8a4a64409cd0c2fd539b
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-06-05 13:20:09 +0530

    button progress

--- src/io/github/boogiemonster1o1/cpslegacy/Main.java
+++ src/io/github/boogiemonster1o1/cpslegacy/Main.java
@@ -5,15 +5,21 @@ import java.awt.*;
 
 public class Main extends JFrame {
 
+    Font defaultFont;
     JMenuBar mainBar = new JMenuBar();
-    JMenu cpsMenu1 = new JMenu();
-    JMenuItem closeItem = new JMenuItem();
-    JLabel headingLabel = new JLabel("Clicks Per Second (Legacy edition)");
+    JMenu cpsMenu1 = new JMenu("Program");
+    JMenuItem closeItem = new JMenuItem("Close Program");
+    JMenuItem visitItem = new JMenuItem("Visit Website");
+    JLabel headingLabel = new JLabel("Clicks Per Second (Legacy edition)",SwingConstants.CENTER);
+    JButton keepClicking = new JButton("Keep Clicking");
+    int clickCount = 0;
+    int numSeconds = 1;
+    int start = 0;
 
     public Main() throws Throwable{
-        this.closeItem.setLabel("Close");
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         this.closeItem.addActionListener(new CloseActionListener());
-        this.cpsMenu1.setLabel("CPS");
+        this.cpsMenu1.add(visitItem);
         this.cpsMenu1.add(closeItem);
         this.mainBar.add(this.cpsMenu1);
         this.setJMenuBar(mainBar);
@@ -21,12 +27,22 @@ public class Main extends JFrame {
         this.setDefaultCloseOperation(3);
         this.setSize(400,400);
 
-        this.headingLabel.setFont(new Font("Arial", Font.BOLD,20));
-        headingLabel.setBounds(10,10,350,25);
+        this.defaultFont = this.closeItem.getFont();
+
+        this.headingLabel.setFont(this.closeItem.getFont().deriveFont(Font.PLAIN).deriveFont(20.0f));
+        this.headingLabel.setBounds(0,10,380,25);
         this.add(headingLabel);
+
+        this.keepClicking.setFont(this.defaultFont.deriveFont(Font.BOLD).deriveFont(30.0f));
+        this.keepClicking.setBounds(10,40,360,180);
+        this.keepClicking.setHorizontalAlignment(SwingConstants.CENTER);
+        this.add(keepClicking);
 
         this.setVisible(true);
     }
+
+
+
     public static void main(String[] args){
         System.out.println("Starting ClicksPerSecond Legacy...");
         try {
@@ -36,7 +52,6 @@ public class Main extends JFrame {
             System.exit(1);
         }
     }
-
     public JMenuBar getJMenuBar() {
         return this.mainBar;
     }
---
+++ src/io/github/boogiemonster1o1/cpslegacy/VisitWebsiteActionListener.java
@@ -0,0 +1,10 @@
+package io.github.boogiemonster1o1.cpslegacy;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VisitWebsiteActionListener implements ActionListener {
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+}
